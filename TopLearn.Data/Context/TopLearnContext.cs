b04296688327646f818ba80fd0fc616e89b6b31f
AmirhosseinAkb <amirbvk1@gmail.com>
2021-12-28 22:30:36 +0330

    AddPackagesAndContext

---
+++ TopLearn.Data/Context/TopLearnContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TopLearn.Data.Context
+{
+    public class TopLearnContext:DbContext
+    {
+        public TopLearnContext(DbContextOptions<TopLearnContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
